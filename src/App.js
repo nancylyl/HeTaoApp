2b6ebf220c81b5ee0324ce19c4591f02cf509b0a
“zhao_wei816” <408612053@qq.com>
2020-08-10 09:19:28 +0800

    赵威

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ import PLogin from './pages/Patient/Login'
 import PRecordLog from './pages/Patient/RecordLog'
 import Error from './components/Error'
 import Discuss from './pages/Doc/Home/discuss/Discuss'
-<<<<<<< HEAD
+
 import activities from './pages/Doc/Home/activities/activities'
 
 import Patient from './pages/Doc/Patient/Patient';
+import allPatient from './pages/Doc/Patient/allPatient';
+import FocusonPatient from './pages/Doc/Patient/FocusonPatient';
+
 import News from './pages/Doc/News/News';
 import MyInfo from './pages/Doc/MyInfo';
-=======
+
 import DiscussDetails from './pages/Doc/Home/discuss/DiscussDetails'
 
 import DocMyPatient from './pages/Doc/MyPatient/'
@@ -27,7 +30,7 @@ import JionDiscuss from './pages/Doc/Home/discuss/JionDiscuss';
 // import Patient from './pages/Doc/Patient/Patient';
 // import News from './pages/Doc/News/News';
 // import MyInfo from './pages/Doc/MyInfo';
->>>>>>> abd2ae15efef3991b70884e67f8f8b64c0f4b09e
+
 
 
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
@@ -46,30 +49,33 @@ function App () {
           <Route path="/patient" exact render={() => <Redirect to={'/Patient/home/index'} />}></Route>
           <Route path="/doc" exact render={() => <Redirect to={'/Doc/home/index'} />}></Route>
           <Route path="/doc/discuss" component={Discuss}></Route>
-<<<<<<< HEAD
+
           <Route path="/doc/activities" component={activities}></Route>
-=======
+
           <Route path="/doc/discussDetails" component={DiscussDetails}></Route>
 
 
           <Route path="/doc/jionDiscuss" component={JionDiscuss}></Route>
 
->>>>>>> abd2ae15efef3991b70884e67f8f8b64c0f4b09e
+          <Route path="/doc/allPatient" component={allPatient}></Route>
+          <Route path="/doc/FocusonPatient" component={FocusonPatient}></Route>
+
           <Route
             path="/doc"
             render={() => (
               <DocIndex>
                 <Route path="/doc/home/index" exact component={DocHomeIndex} />
                 <Route path="/doc/patient" component={Patient}></Route>
+
                 <Route path="/doc/news" component={News}></Route>
-<<<<<<< HEAD
+
                 <Route path="/doc/myInfo/index" component={MyInfo}></Route>
-=======
-                <Route path="/doc/myInfo/index" component={MyInfo}></Route> */}
+
+                <Route path="/doc/myInfo/index" component={MyInfo}></Route>
 
                 <Route path="/doc/home/mypatient" exact component={DocMyPatient} />
                 <Route path="/doc/home/mypatientdetail" exact component={DocMyPatientDetail} />
->>>>>>> abd2ae15efef3991b70884e67f8f8b64c0f4b09e
+
               </DocIndex>
             )}
           >
