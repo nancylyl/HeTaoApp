5a441878b5dde031bc67d71bc67d690aa62b5c8d
zhangxu668 <1505046353@qq.com>
2020-08-12 17:46:21 +0800

    1

--- src/pages/Doc/Home/discuss/Discuss.js
+++ src/pages/Doc/Home/discuss/Discuss.js
@@ -33,7 +33,7 @@ export default class Discuss extends Component {
           method: "get",
         })
           .then((res) => {
-            // console.log(res)
+            console.log(res)
             if (res.status== 200) {
               this.setState({
                 discussList: res.data.data,
@@ -71,6 +71,7 @@ export default class Discuss extends Component {
             return <div key={index} className={styles.list} onClick={this.goToUrl.bind(this,item)}>
                     <h1>{item.discussName}</h1>
                     <p>{item.explain}</p>
+                    <p>参会主持：{item.doctoId}&nbsp;|&nbsp;{item.hospital}{item.subject}{item.profession}</p> 
                     {/* <p>参会主持：{item.doctor[0].name}&nbsp;|&nbsp;{item.doctor[0].hospital}{item.doctor[0].subject}{item.doctor[0].profession}</p> */}
                     <p>时间：{moment(item.discussStart).format('YY/MM/DD')}</p>
                     <p>已报名人数：{item.bNumber}人&nbsp;&nbsp;（{item.joinNumber}人）<span>{item.chargeType}</span></p>
@@ -88,7 +89,7 @@ export default class Discuss extends Component {
                 <div className={styles.topTitle}><Icon type={'left'}  className={styles.icon} onClick={this.goback}/>病历探讨</div>
                 <Carousel
                     autoplay
-                    // infinite
+                    infinite
                     // beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
                     // afterChange={index => console.log('slide to', index)}
                     className={styles.banner}
