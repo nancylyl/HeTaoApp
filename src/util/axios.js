171bf2ccd4f9cf3fd5a787561666873b8f04831c
e <yalan.liu@macrowing.com>
2020-08-12 17:45:17 +0800

    axios

--- src/util/axios.js
+++ src/util/axios.js
@@ -2,14 +2,8 @@ import Axios from 'axios'
 var baseURL = ''
 export default function axios (config) {
   if (config.isDev != undefined && config.isDev == 1) {
-    // baseURL = "http://118.190.245.9:8080/htr"
-<<<<<<< HEAD
-    baseURL = "http://172.16.2.75:8080/htr"
-=======
-    // baseURL = "http://172.16.2.12:8080/htr" 
-    // baseURL = "http://172.16.2.75:8080/htr" //病历探讨
-    baseURL = "http://172.16.2.78:8080/htr" 
->>>>>>> c7ecc556d5081e1441998e4ad7348ff82b525f2e
+    baseURL = "http://118.190.245.9:8080/htr"
+    // baseURL = "http://172.16.2.75:8080/htr"
   }
   else {
     baseURL = "https://www.fastmock.site/mock/63908e19f8683a898abc0a03e1010b59"
